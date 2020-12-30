2c9c069e35cf76d36018c66d9822b4dfefe14277
William Porto <william.porto@nave.rs>
2020-12-30 01:21:53 -0300

    feature: refactoring MainActivity and adding Fragments

--- app/src/main/java/com/powilliam/anothertodoapp/TodosFragment.kt
+++ app/src/main/java/com/powilliam/anothertodoapp/TodosFragment.kt
@@ -3,57 +3,41 @@ package com.powilliam.anothertodoapp
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import com.powilliam.anothertodoapp.databinding.FragmentTodosBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [TodosFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TodosFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentTodosBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_todos, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_todos, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TodosFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TodosFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setToolbarOnMenuItemClickListener()
+    }
+
+    private fun setToolbarOnMenuItemClickListener() {
+        binding.toolbar.setOnMenuItemClickListener { menuItem: MenuItem ->
+            when(menuItem.itemId) {
+                R.id.write_todo -> navigateToWriteTodoBottomSheetFragment()
+                else -> false
             }
+        }
+    }
+
+    private fun navigateToWriteTodoBottomSheetFragment(): Boolean {
+        val controller = findNavController()
+        controller.navigate(R.id.action_todosFragment_to_writeTodoBottomSheetFragment)
+        return true
     }
 }
